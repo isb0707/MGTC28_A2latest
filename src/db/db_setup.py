99ad0991cc42c1fc626ad9a4054e667222fe070a
lexiangwang8@gmail.com <isb0707>
2024-11-08 14:52:06 -0500

    4steps

--- src/db/db_setup.py
+++ src/db/db_setup.py
@@ -174,17 +174,20 @@ def ingest_csv_data(filename: str):
     #2 Use pandas to load the CSV 'filename' into a dataframe which we'll call 'df'. If you are doing
     # any cleaning of the dataframe after loading it, make a new variable called 'cleaned_df' and store the cleaned
     # dataframe there.
-
+    df =pd.read_csv('legacy_employees.csv')
+    print(df)
     #3 Now that you have the CSV data loaded into a dataframe, you need to insert the data into the SQL database.
     # Using the dataframe that you created above, as well as the database connection that you have instantiated,
     # use one of the helper functions above to insert the data into the database.
-
+    df = remove_unnamed_columns(df)
+    insert_employee_data_into_db(db_connection, df)
     #4 Now to show that you've finished processing the CSV file, move the file over to the hist folder 
     # Hint: use shutil.move and read the parameters it takes)
     # Hint: I personally use f-strings to use variables in the middle of strings, so in the destination path to move the file, I'd use f"hist/{filename}"
     # If you want the formal definition of what an f-string does: https://www.geeksforgeeks.org/formatted-string-literals-f-strings-python/
-    
-    pass # <- REMOVE THIS WHEN YOU IMPLEMENT YOUR FUNCTION
+    filename = 'legacy_emplyess.csv'
+    destination = f'hist/{filename}'
+    shutil.move(filename,destination)
 
 def print_employee_dataframe():
     """Connects to the database, puts the Employee table into a dataframe, and then prints the dataframe.
