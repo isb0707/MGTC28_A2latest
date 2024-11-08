86101fa09d623b67f94415275fa95928ec5985af
lexiangwang8@gmail.com <isb0707>
2024-11-08 14:17:04 -0500

    first chart

--- src/main.py
+++ src/main.py
@@ -42,8 +42,10 @@ def get_avg_salary_by_job_title(employee_salary_df: pd.DataFrame, selected_title
 #TODO: MAKE THIS USING THE MODEL FUNCTION ABOVE (Copy pasting is your friend here)
 # HINT: The country column in the dataframe is 'Country'
 def get_avg_salary_by_country(employee_salary_df: pd.DataFrame, selected_countries: list):
-    pass
-
+    filtered_df = employee_salary_df[employee_salary_df['Country'].isin(selected_countries)]
+    avg_salary_by_job_title = filtered_df.groupby('Country')['YearlyCompensation'].mean().reset_index()
+    st.subheader("Average Salary by Country")
+    st.bar_chart(avg_salary_by_job_title, x = 'Country', y = 'YearlyCompensation')
 #TODO: MAKE THIS USING THE MODEL FUNCTION ABOVE (Copy pasting is your friend here)
 def get_num_employees_by_country(employee_salary_df: pd.DataFrame, selected_countries: list):
     pass
